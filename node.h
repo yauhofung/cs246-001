74e6eacdc5ce175787debf5b683b695aa02fa2f2
yauhofung <yauho.fung@student.mec.cuny.edu>
2019-09-19 18:06:15 -0400

    Node

--- node.h
+++ node.h
@@ -1,9 +1,55 @@
 #ifndef NODE_H
 #define NODE_H
 
-template<class T>
-class Node
-{   T data;
-    Node<T>* link;};
+#include <iostream>
+#include <string>
+#include <sstream>
 
+namespace ds
+{
+	template <class T>
+	class Node
+	{
+	private:
+		T data;
+		Node<T>* link;
+
+		Node(const Node<T>& obj);
+		Node& operator=(const Node<T>& rhs);
+	public:
+		Node() : Node(T(),NULL) {}
+
+		Node(const T& data) : Node(data,NULL) {}
+
+		Node(const T& data,Node<T>* link) : data(data), link(link) {}
+
+		~Node() {}
+	
+		T& GetData() {return data;}
+
+		const T& GetData() const {return data;}
+
+		Node<T>* GetLink() {return link;}
+
+		const Node<T>* GetLink() const {return link;}
+
+		void SetData(const T& data) {this->data = data;}
+
+		void SetLink(Node<T>* link) {this->link = link;}
+
+		std::string ToString() const
+		{
+			std::stringstream out;
+			out << data;
+			return out.str();
+		}
+
+		friend std::ostream& operator<<(std::ostream& out,const Node<T>& obj)
+		{
+			out << obj.ToString();
+			return out;
+		}
+	};
+}
+		
 #endif
---
+++ nodemain.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "node.h"
+using namespace ds;
+using namespace std;
+
+template<class T>
+void append(Node<T>*& root,const T& data)
+{   if(root==NULL)
+    {   root=new Node<T>(data);}
+    else
+    {   Node<T>* tmp=root;
+        while(tmp->GetLink()!=NULL)
+        {   tmp=tmp->GetLink();}
+        tmp->SetLink(new Node<T>(data));}}
+
+template<class T>
+void traverse(Node<T>* root)
+{   cout<<"[";
+    Node<T>* tmp=root;
+    while(tmp!=NULL)
+    {   cout<<tmp->GetData();
+        if(tmp->GetLink()!=NULL)
+        {   cout<<",";}
+        tmp=tmp->GetLink();}
+    cout<<"]";}
+
+template<class T>
+void Clear(Node<T>*& root)
+{   Node<T>* tmp;
+    while(root!=NULL)
+    {   tmp=root;
+        root=root->GetLink();
+        delete tmp;
+        tmp=NULL;}}
+
+int main()
+{   srand(time(NULL));
+    Node<int>* root=NULL;
+    int n=10;
+    for(int i=0;i<n;i+=1)
+    {   append(root,rand()%24+5);
+        traverse(root);
+        cout<<endl;}
+    return 0;}
