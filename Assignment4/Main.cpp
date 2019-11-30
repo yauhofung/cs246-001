07bc18b67271a52867d44b5b786b3f3942365c87
yauhofung <yauho.fung@student.mec.cuny.edu>
2019-11-29 21:30:16 -0500

    Updated Assignment4

--- Assignment4/Main.cpp
+++ Assignment4/Main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <sstream>
 #include "FrequencyTable.h"
+#include "Spooler.h"
 using namespace std;
 
 string CharacterCount(ifstream& ReadFile)
@@ -10,4 +11,31 @@ string CharacterCount(ifstream& ReadFile)
 	getline(ReadFile,str);
 	return str;}
 
+void Printer(ofstream& WriteFile)
+{	int option;
+	Spooler spooler;
+	while(option!=4)
+	{	cout<<"1. Add a job and count.\n";
+		cout<<"2. Display query.\n";
+		cout<<"3. Execute batch.\n";
+		cout<<"4. Quit.\n";
+		cout<<"Select an option: ";
+		getline(cin,option);
 
+		if(option==1)
+		{	string path;
+			int count;
+			cout<<"Enter a file path: ";
+			getline(cin,path);
+			cout<<"Enter a count: ";
+			getline(cin,count);
+			spooler.AddJob(path,count);}
+
+		else if(option==2)
+		{	cout<<spooler<<"\n";}
+
+		else if(option==3)
+		{	string str;
+			Pair<string,int> job=spooler.ExecuteJob();
+			ifstream ReadFile(job.GetKey());
+			
--- Assignment4/Spooler.h
+++ Assignment4/Spooler.h
@@ -13,4 +13,71 @@ class Spooler
 		Node<Pair<string,int> >* front;
 		Node<Pair<string,int> >* back;
 	public:
-		
+		Spooler()
+		{	front=NULL;
+			back=NULL;}
+		Spooler(const Spooler& obj)
+		{	Node<Pair<string,int> >* tmp=obj.front;
+			if(tmp!=NULL)
+			{	front=new Node<Pair<string,int> >(tmp->GetData());
+				back=front;
+				tmp=tmp->GetLink();
+				while(tmp->NULL)
+				{	back->SetLink(new Node<Pair<string,int> >(tmp->GetData()));
+					tmp=tmp->GetLink();
+					back=back->GetLink();}}}
+		Spooler& operator=(const Spooler& rhs)
+		{	if(this!=&rhs)
+			{	Clear(front);
+				back=NULL;
+				Node<Pair<string,int> >* tmp=rhs.front;
+				if(tmp!=NULL)
+				{	front=new Node<Pair<string,int> >(tmp->GetData());
+					back=front;
+					tmp=tmp->GetLink();
+					while(tmp!=NULL)
+					{	back->SetLink(new Node<Pair<string,int> >(tmp->GetData()));
+						tmp=tmp->GetLink();
+						back=back->GetLink();}}}
+			return *this;}
+		~Spooler()
+		{	Clear(front);
+			front=NULL;
+			back=NULL;}
+
+		void AddJob(Pair<string,int> job)
+		{	if(front==NULL)
+			{	front=new Node<Pair<string,int> >(job);
+				back=front;}
+			else
+			{	back->SetLink(new Node<Pair<string,int> >(job));
+				back=back->GetLink();}}
+
+		Pair<string,int> ExecuteJob()
+		{	if(front==NULL)
+			{	return NULL;}
+			else
+			{	Pair<string,int> job=front->GetData();
+				Node<Pair<string,int> >* tmp=front;
+				front=front->GetLink();
+				delete tmp;
+				tmp=NULL;
+				if(front==NULL)
+				{	back=NULL;}
+				return job;}}
+
+		string Query()
+		{	stringstream out;
+			out<<"[";
+			for(Node<Pair<string,int> >*tmp=front;tmp!=NULL;tmp=tmp->GetLink())
+			{	out<<tmp->GetData();
+				if(tmp->GetLink()!=NULL)
+				{	out<<",";}}
+			out<<"]";
+			return out.str();}
+
+		friend ostream& operator<<(ostream& out,const Spooler& obj)
+		{	out<<obj.ToString();
+			return out;}};
+
+#endif
